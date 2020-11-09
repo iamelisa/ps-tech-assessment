f9a7c0d1bb0ebea81f1df94d6be7995a6cae5698
iamelisa <74167926+iamelisa@users.noreply.github.com>
2020-11-08 21:22:16 -0800

    Initial Commit

---
+++ Get-FirstRecurringCharacter.ps1
@@ -0,0 +1,18 @@
+﻿
+function Get-FirstRecurringCharacter {
+    param([string] $InputString)
+    $charArray = $InputString.ToCharArray();
+    $hash = @{}
+
+    foreach ($item in $charArray) {
+    if($hash.ContainsKey($item)) {
+        return $item
+        }
+    $hash.Add($item, $true)
+    }
+}
+
+Get-FirstRecurringCharacter("")
+Get-FirstRecurringCharacter("123456")
+Get-FirstRecurringCharacter("abcdedcba")
+Get-FirstRecurringCharacter(".++()******")
